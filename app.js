17ea81f824ca0594f057ad492f464ccfb0ca9088
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-04-25 21:11:31 +0200

    Add book and library classes

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
-const bookArray = JSON.parse(localStorage.getItem('bookArray')) || [];
-
-function updateLocalStorage() {
-  localStorage.setItem('bookArray', JSON.stringify(bookArray));
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-function displayBooks() {
-  const bookList = document.getElementById('book-list');
+class Library{
+  constructor(){
+    this.bookArray = JSON.parse(localStorage.getItem('bookArray')) || [];
+    this.bookList = document.getElementById('book-list');
+    this.authorInput = document.getElementById('author');
+    this.bookInput = document.getElementById('book');
+    this.addButton = document.getElementById('add');
 
-  bookList.innerHTML = '';
-  bookArray.forEach((book, index) => {
+  this.addButton.addEventListener('click', () => {
+    const author = this.authorInput.value;
+    const book = this.bookInput.value;
+    this.addBook(author, book);
+    this.authorInput.value = '';
+    this.bookInput.value = '';
+  });
+  this.displayBooks();
+};
+updateLocalStorage() {
+  localStorage.setItem('bookArray', JSON.stringify(this.bookArray));
+}
+displayBooks(){
+  this.bookList.innerHTML = '';
+  this.bookArray.forEach((book, index) => {
     const div = document.createElement('div');
     div.setAttribute('id', 'div');
-    bookList.appendChild(div);
-    const titleB = document.createElement('h3');
-    titleB.innerHTML = `${book.book}`;
-    div.appendChild(titleB);
-    const authorB = document.createElement('h3');
-    authorB.innerHTML = `${book.author}`;
-    div.appendChild(authorB);
-    const buttonB = document.createElement('h3');
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = 'Remove';
-    removeButton.addEventListener('click', () => {
+     this.bookList.appendChild(div);
+      const titleB = document.createElement('h3');
+      titleB.innerHTML = `${book.author} by ${book.title}`;
+      div.appendChild(titleB);
+      const buttonB = document.createElement('h3');
+      const removeButton = document.createElement('button');
+      removeButton.innerHTML = 'Remove';
+      removeButton.addEventListener('click', () => {
       buttonB.parentElement.remove();
-      bookArray.splice(index, 1);
-      updateLocalStorage();
+      this.bookArray.splice(index, 1);
+      this.updateLocalStorage();
+      });
+      buttonB.appendChild(removeButton);
+      div.appendChild(buttonB);
+      const hr = document.createElement('hr');
+      div.appendChild(hr);
     });
-    buttonB.appendChild(removeButton);
-    div.appendChild(buttonB);
-    const hr = document.createElement('hr');
-    div.appendChild(hr);
-  });
+  }
 }
-function addBook(author, book) {
-  bookArray.push({ author, book });
-  updateLocalStorage();
-  displayBooks();
-}
-
-const authorInput = document.getElementById('author');
-const bookInput = document.getElementById('book');
-const addButton = document.getElementById('add');
-addButton.addEventListener('click', () => {
-  const author = authorInput.value;
-  const book = bookInput.value;
-  addBook(author, book);
-  authorInput.value = '';
-  bookInput.value = '';
-});
 updateLocalStorage();
 displayBooks();
