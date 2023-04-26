77f68391fd7335caa61c810e992b24a7a5eb3bf9
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-04-26 19:31:32 +0200

    Change js file

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 /* eslint-disable max-classes-per-file */
 
+const booksLink = document.querySelector('#books-link');
+const bookInputsLink = document.querySelector('#bookInputs-link');
+const contactLink = document.querySelector('#contact-link');
+const booksSection = document.querySelector('#books');
+const bookInputsSection = document.querySelector('#bookInputs');
+const contactSection = document.querySelector('#contact');
+
+booksLink.addEventListener('click', () => {
+  booksSection.style.display = 'flex';
+  bookInputsSection.style.display = 'none';
+  contactSection.style.display = 'none';
+});
+
+bookInputsLink.addEventListener('click', () => {
+  booksSection.style.display = 'none';
+  bookInputsSection.style.display = 'flex';
+  contactSection.style.display = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+  booksSection.style.display = 'none';
+  bookInputsSection.style.display = 'none';
+  contactSection.style.display = 'flex';
+});
+
 class Book {
   constructor(title, author) {
     this.title = title;
